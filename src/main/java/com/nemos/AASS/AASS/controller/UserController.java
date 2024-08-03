7c43c11ecf5613c2a418d52ec8d2f76519de2b3a
Gautham-94 <59800427+Gautham-94@users.noreply.github.com>
2024-08-03 10:29:43 +0530

    new endpoint for login

--- src/main/java/com/nemos/AASS/AASS/controller/UserController.java
+++ src/main/java/com/nemos/AASS/AASS/controller/UserController.java
@@ -3,6 +3,9 @@ package com.nemos.AASS.AASS.controller;
 import com.nemos.AASS.AASS.model.User;
 import com.nemos.AASS.AASS.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +17,25 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
 
     @PostMapping("register")
     public User register(@RequestBody User user){
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return "Success";
+        }
+        else {
+            return "Login Failed";
+        }
     }
 }
